45954a8f0152f034cf438014b84e6aade19a95b3
Mostafa <mostafaorashy5@gmail.com>
2025-05-05 01:00:32 +0300

    Merged

--- client/src/main/java/client/ClientApp.java
+++ client/src/main/java/client/ClientApp.java
@@ -6,8 +6,8 @@ import client.documentFrames.DocumentsFrame;
 import client.loginFrames.LoginFrame;
 
 public class ClientApp {
-    public static final String SERVER_HOST = "192.168.100.249";; // Server host address
-    public static final int PORT = 12345;
+    public static final String SERVER_HOST = "serveo.net";; // Server host address
+    public static final int PORT = 37305;
     public static void main(String[] args) {
         // Launch the login screen when the application starts
         SwingUtilities.invokeLater(() -> {
